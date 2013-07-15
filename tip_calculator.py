defae9267821b0959d15ee2dad122024b06b4c33
Devon Campbell <raddevon@gmail.com>
2013-07-15 16:54:33 -0400

    Switched to new string formatting

--- tip_calculator.py
+++ tip_calculator.py
@@ -10,8 +10,8 @@ meal_with_tax = meal + tax_value
 tip_value = meal_with_tax * tip
 total = meal_with_tax + tip_value
 
-print "The base cost of your meal was $%.2f" % meal
-print "You need to pay $%.2f for tax" % tax_value
-print "Tipping at a rate of %s%%, you should leave $%.2f for a tip." \
-    % (tax * 100, tip_value)
-print "The grand total of your meal is $%.2f" % total
+print "The base cost of your meal was ${:.2f}".format(meal)
+print "You need to pay ${:.2f} for tax".format(tax_value)
+print "Tipping at a rate of {:.2%}, you should leave ${:.2f} for a tip." \
+    .format(tax, tip_value)
+print "The grand total of your meal is ${:.2f}".format(total)
